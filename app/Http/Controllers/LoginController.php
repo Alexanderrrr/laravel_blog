961ee18ef513e5b29853bf2b58a8db2a3d1ea478
Alexanderrrr <acons1987@gmail.com>
2018-10-25 21:02:54 +0200

    Log in page and back

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,9 +6,30 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+
+    public function index()
+    {
+      return view('login.index');
+    }
+
+
+    public function login()
+    {
+      if(!auth()->attempt(request(['email', 'password']))) {
+          return back()->withErrors([
+            'message' => 'Omasijo si code'
+          ]);
+      }
+
+
+      return redirect('/posts');
+    }
+
+
     public function logout()
     {
       auth()->logout();
-      return redirect('/posts');
+
+      return redirect('/login');
     }
 }
